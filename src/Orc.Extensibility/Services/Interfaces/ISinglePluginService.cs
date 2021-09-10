69bd436609248ba1f808840ed2ffaadba39b8a8d
Dmitry Fomenko <fomedima@gmail.com>
2021-09-10 10:46:21 +0300

    GUM-176 argument renaming, approval test

--- src/Orc.Extensibility/Services/Interfaces/ISinglePluginService.cs
+++ src/Orc.Extensibility/Services/Interfaces/ISinglePluginService.cs
@@ -5,6 +5,6 @@
     public interface ISinglePluginService : IPluginService
     {
         Task<IPlugin> ConfigureAndLoadPluginAsync(string expectedPlugin, string defaultPlugin);
-        Task SetFallbackPluginAsync(IPluginInfo defaultPlugin);
+        Task SetFallbackPluginAsync(IPluginInfo fallbackPlugin);
     }
 }

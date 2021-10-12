2ff31e80bcf6d9d78fc6e1a5990ed354a586fa70
Artem Saikin <art.saikin@gmail.com>
2021-10-12 20:40:14 +0300

    Fix possible issues with missing paths

--- src/Orc.Extensibility/Services/PluginFinderBase.cs
+++ src/Orc.Extensibility/Services/PluginFinderBase.cs
@@ -486,12 +486,14 @@ namespace Orc.Extensibility
                     {
                         if (existingVersion != version)
                         {
+                            // Important: just log, but still add the path
                             Log.Warning($"Already loaded '{fileName}' version '{existingVersion}', but also found runtime assembly '{version}'. The already loaded assembly will be used to investigate '{assemblyPath}'");
-                            continue;
                         }
                     }
-
-                    assemblyVersions[fileName] = version;
+                    else
+                    {
+                        assemblyVersions[fileName] = version;
+                    }
 
                     paths.Add(runtimeAssembly.Location);
                 }
@@ -522,8 +524,6 @@ namespace Orc.Extensibility
                 }
             }
         }
-
-
 
         protected virtual bool ShouldIgnoreAssembly(string assemblyPath)
         {

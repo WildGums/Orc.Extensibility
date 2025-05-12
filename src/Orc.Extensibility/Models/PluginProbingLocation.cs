a91aaa08477a1313d5e4d8ad9f19167fa0fc0b80
Geert van Horrik <geert@catenalogic.com>
2025-05-12 11:36:36 +0200

    Add PluginProbingLocation to customize recursive plugin probing

---
+++ src/Orc.Extensibility/Models/PluginProbingLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace Orc.Extensibility
+{
+    public class PluginProbingLocation
+    {
+        public PluginProbingLocation()
+        {
+            IsRecursive = true;
+        }
+
+        public required string Location { get; set; }
+
+        public bool IsRecursive { get; set; }
+    }
+}
--- src/Orc.Extensibility/Services/PluginLocationsProvider.cs
+++ src/Orc.Extensibility/Services/PluginLocationsProvider.cs
@@ -19,6 +19,7 @@ public class PluginLocationsProvider : IPluginLocationsProvider
         _appDataService = appDataService;
     }
 
+    [ObsoleteEx(ReplacementTypeOrMember = nameof(GetPluginLocations), TreatAsErrorFromVersion = "5.0", RemoveInVersion = "6.0")]
     public virtual IReadOnlyList<string> GetPluginDirectories()
     {
         var directories = new List<string>();
@@ -42,6 +43,70 @@ public class PluginLocationsProvider : IPluginLocationsProvider
             !directories.Any(x => x.EqualsIgnoreCase(appDirectory)))
         {
             directories.Add(appDirectory);
+        }
+
+        return directories;
+    }
+
+    public virtual IReadOnlyList<PluginProbingLocation> GetPluginLocations()
+    {
+        var directories = new List<PluginProbingLocation>();
+
+        var pluginsDirectory = System.IO.Path.Combine(_appDataService.GetApplicationDataDirectory(ApplicationDataTarget.UserRoaming), "plugins");
+        if (ValidateDirectory(pluginsDirectory) &&
+            !directories.Any(x => x.Location.EqualsIgnoreCase(pluginsDirectory)))
+        {
+            // Note: this is the default, and should be added as recursive since
+            // chances are very high that the user will add plugins in subfolders
+            directories.Add(new PluginProbingLocation
+            {
+                Location = pluginsDirectory,
+                IsRecursive = true
+            });
+        }
+
+        var currentDirectory = Environment.CurrentDirectory;
+        if (ValidateDirectory(currentDirectory) &&
+            !directories.Any(x => x.Location.EqualsIgnoreCase(currentDirectory)))
+        {
+            // Note: don't add as recursive, if there is a "plugins" subfolder,
+            // it should be added manually for better performance
+            directories.Add(new PluginProbingLocation
+            {
+                Location = pluginsDirectory,
+                IsRecursive = false
+            });
+        }
+
+        var appDirectory = AssemblyHelper.GetRequiredEntryAssembly().GetDirectory();
+        if (ValidateDirectory(appDirectory) &&
+            !directories.Any(x => x.Location.EqualsIgnoreCase(appDirectory)))
+        {
+            // Note: don't add as recursive, if there is a "plugins" subfolder,
+            // it should be added manually for better performance
+            directories.Add(new PluginProbingLocation
+            {
+                Location = appDirectory,
+                IsRecursive = false
+            });
+        }
+
+        // Ensure backwards compatibility with the old method name, but we add it at the end
+        // so that the new method is preferred over the old one
+        var oldPluginDirectories = GetPluginDirectories();
+
+        foreach (var oldPluginDirectory in oldPluginDirectories)
+        {
+            if (ValidateDirectory(oldPluginDirectory) &&
+                !directories.Any(x => x.Location.EqualsIgnoreCase(oldPluginDirectory)))
+            {
+                // Note: for backwards compatibility, we add the old plugin directory as recursive
+                directories.Add(new PluginProbingLocation
+                {
+                    Location = oldPluginDirectory,
+                    IsRecursive = true
+                });
+            }
         }
 
         return directories;

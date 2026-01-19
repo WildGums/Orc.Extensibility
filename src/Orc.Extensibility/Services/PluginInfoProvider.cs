cfcd7941daa3949af234af75ddf7a6b1305b5cc6
Geert van Horrik <geert@catenalogic.com>
2026-01-20 00:44:36 +0100

    Update

--- src/Orc.Extensibility/Services/PluginInfoProvider.cs
+++ src/Orc.Extensibility/Services/PluginInfoProvider.cs
@@ -6,9 +6,6 @@ public class PluginInfoProvider : IPluginInfoProvider
 {
     public virtual IPluginInfo GetPluginInfo(string location, Type type, Type? registrarType)
     {
-        ArgumentNullException.ThrowIfNull(location);
-        ArgumentNullException.ThrowIfNull(type);
-
         return new PluginInfo(location, type, registrarType);
     }
 }
